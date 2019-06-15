ece991383502ebd2b9fe021f5b5ae2a48dfdc2ec
Lay <brownsugar.idv.tw@gmail.com>
2019-06-15 15:25:58 +0800

    use kwgh function

--- assets/js/events/default.js
+++ assets/js/events/default.js
@@ -1,14 +1,15 @@
-const kwghTemplate = `
-<div id="kwgh" class="kwgh-wrap kwgh-error">
-  <input type="checkbox" id="kwgh-alert" class="alert-state" />
-  <div class="alert alert-danger dismissible">
-    <div class="content">
-      <h4>KartWebGameHelper</h4>
-      <p>This event is not supported yet. <a href="${KWGH_URL}" target="_blank">Request to update?</a></p>
+(() => {
+  const kwghTemplate = `
+  <div id="kwgh" class="kwgh-wrap kwgh-error">
+    <input type="checkbox" id="kwgh-alert" class="alert-state" />
+    <div class="alert alert-danger dismissible">
+      <div class="content">
+        <h4>KartWebGameHelper</h4>
+        <p>This event is not supported yet. <a href="${KWGH_URL}" target="_blank">Request to update?</a></p>
+      </div>
+      <label class="btn-close" for="kwgh-alert">X</label>
     </div>
-    <label class="btn-close" for="kwgh-alert">X</label>
   </div>
-</div>
-`
-
-document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend', kwghTemplate)
+  `
+  kwgh.el.append('body', kwghTemplate)
+})()
