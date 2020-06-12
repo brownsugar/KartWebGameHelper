79d84057b0e993aedf07676a2de1522bf10a15df
Lay <brownsugar.idv.tw@gmail.com>
2020-06-13 00:37:55 +0800

    fix typo

--- assets/js/events/default.js
+++ assets/js/events/default.js
@@ -6,7 +6,7 @@
       <div class="alert alert-danger dismissible">
         <div class="content">
           <h4>KartWebGameHelper</h4>
-          <p>This event is not supported yet. <a href="https://kinf.cc/2sS1uoD" target="_blank">Request to update?</a></p>
+          <p>This event is not supported yet. <a href="https://kinf.cc/2sS1uoD" target="_blank">Request an update?</a></p>
         </div>
         <label class="btn-close" for="kwgh-alert" onclick="kwgh.setDismiss('${path}')">X</label>
       </div>
